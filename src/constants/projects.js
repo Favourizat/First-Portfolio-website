d381f1e30908f3d20cb1a1fb866d743d24d82f9f
Favourizat <iwuohafavour06@gmail git config --global user.name Favourizat.com>
2026-09-25 10:00:07 +0100

    Update Mare Vista website

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -30,15 +30,49 @@ const projects = [
 
     live: "https://lumeabrand.vercel.app/",
   },
-
   {
     id: 2,
+    title: "Maré Vista",
+
+    image: "/projects/mare-vista-portfolio-image.png",
+
+    Description:
+
+      "A responsive luxury hotel booking website built with Next.js, React, Tailwind CSS, MongoDB, and Paystack, featuring room browsing, category filtering, room detail pages, real-time room availability checking, guest booking, additional hotel services, secure user authentication, booking management, and online payments. Maré Vista provides a seamless and elegant booking experience across desktop and mobile devices, with a complete reservation workflow, payment verification, booking confirmation, and a secure backend for managing users and reservations.",
+
+    Technologies:
+
+      [
+        "Next.js",
+        "React",
+        "JavaScript (ES6+)",
+        "Tailwind CSS",
+        "Next.js App Router",
+        "MongoDB",
+        "Mongoose",
+        "NextAuth.js",
+        "Paystack",
+        "EmailJS",
+        "Lucide React",
+        "Git",
+        "GitHub",
+        "Vercel"
+      ],
+
+
+    github: "https://github.com/Favourizat/mare-vista",
+
+    live: "https://mare-vista-oq888lh4o-favouriza.vercel.app/",
+  },
+
+  {
+    id: 3,
     title: "Livewell",
 
     image: "/projects/Livewell-portfolio-picture.png",
 
     description:
-     "A responsive real estate website built with React, Tailwind CSS, and Firebase, designed to provide a smooth and intuitive property discovery experience. The platform features dynamic property listings, detailed property pages, property search and filtering, user authentication, and a favorites system that allows users to save properties for easy access. It also includes responsive layouts and reusable components to ensure a consistent experience across all devices.",
+      "A responsive real estate website built with React, Tailwind CSS, and Firebase, designed to provide a smooth and intuitive property discovery experience. The platform features dynamic property listings, detailed property pages, property search and filtering, user authentication, and a favorites system that allows users to save properties for easy access. It also includes responsive layouts and reusable components to ensure a consistent experience across all devices.",
     technologies: [
       "React",
       "Vite",
@@ -61,60 +95,60 @@ const projects = [
     live: "https://livewell-red.vercel.app/",
   },
 
-  {
-    id: 3,
-    title: "TripSphere",
+  // {
+  //   id: 3,
+  //   title: "TripSphere",
 
-    image: "/projects/TripSphere-Portfolio.png",
+  //   image: "/projects/TripSphere-Portfolio.png",
 
-    description:
-      "A responsive travel booking website built with React, Tailwind CSS, featuring holiday packages, popular destinations, flight and hotel search, tour details, booking functionality, and a seamless experience across desktop and mobile devices.",
+  //   description:
+  //     "A responsive travel booking website built with React, Tailwind CSS, featuring holiday packages, popular destinations, flight and hotel search, tour details, booking functionality, and a seamless experience across desktop and mobile devices.",
 
-    technologies: [
-      "React.js",
-      "JavaScript(ES6 +)",
-      "Vite",
-      "Tailwind CSS",
-      "React",
-      "Lucide React",
-      "React Icons",
-      "React Toastify",
-      "EmailJS",
-      "Git",
-      "GitHub",
-      "React Router",
-      "Vercel",
-    ],
+  //   technologies: [
+  //     "React.js",
+  //     "JavaScript(ES6 +)",
+  //     "Vite",
+  //     "Tailwind CSS",
+  //     "React",
+  //     "Lucide React",
+  //     "React Icons",
+  //     "React Toastify",
+  //     "EmailJS",
+  //     "Git",
+  //     "GitHub",
+  //     "React Router",
+  //     "Vercel",
+  //   ],
 
-    github: "https://github.com/Favourizat/TripSphere.git",
+  //   github: "https://github.com/Favourizat/TripSphere.git",
 
-    live: "https://trip-sphere-vert.vercel.app/",
-  },
+  //   live: "https://trip-sphere-vert.vercel.app/",
+  // },
 
-  {
-    id: 4,
-    title: "moviela-movie-search-app",
+  // {
+  //   id: 4,
+  //   title: "moviela-movie-search-app",
 
-    image: "/projects/Moviela.png",
+  //   image: "/projects/Moviela.png",
 
-    description:
-      "A responsive movie discovery application built with React and Tailwind CSS, allowing users to search movies, view detailed information, manage favorites, and authenticate securely using Firebase.",
+  //   description:
+  //     "A responsive movie discovery application built with React and Tailwind CSS, allowing users to search movies, view detailed information, manage favorites, and authenticate securely using Firebase.",
 
-    technologies: [
-      "React",
-      "Vite",
-      "Tailwind CSS",
-      "JavaScript (ES6+)",
-      "React Router",
-      "Firebase Authentication",
-      "TMDB API",
-      "Git & GitHub",
-      "Vercel"
-    ],
-    github: "https://github.com/Favourizat/Moviela.git",
+  //   technologies: [
+  //     "React",
+  //     "Vite",
+  //     "Tailwind CSS",
+  //     "JavaScript (ES6+)",
+  //     "React Router",
+  //     "Firebase Authentication",
+  //     "TMDB API",
+  //     "Git & GitHub",
+  //     "Vercel"
+  //   ],
+  //   github: "https://github.com/Favourizat/Moviela.git",
 
-    live: "https://moviela-weld.vercel.app/",
-  },
+  //   live: "https://moviela-weld.vercel.app/",
+  // },
 
   // {
   //   id: 4,
@@ -138,7 +172,7 @@ const projects = [
 
   //   live: "https://vastu-spaze-t4py.vercel.app",
   // },
- 
+
 ]
 
 export default projects;
